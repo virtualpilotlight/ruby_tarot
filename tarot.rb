2a69e07dc228cd2437588bb88fa9aafce6b65193
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2022-09-22 23:06:17 -0700

    Create tarot.rb
    array need to be edited further just wanted it to be saved here so I can get at it later.

---
+++ tarot.rb
@@ -0,0 +1,81 @@
+all_cards = [ 
+["|0| - the fool - upright", "|0| - the fool - inveted"],
+["|i| - the magician - upright", "|i| - the magician"], 
+["|ii| - the high priestess", "|ii| - the high priestess"], 
+["|iii| - the empress", "|iii| - the empress"], 
+["|iv| - the emperor", "|iv| - the emperor"],
+["|v| -the hierophant", "|v| -the hierophant"],
+["|vi| - the lovers", "|vi| - the lovers"],
+["|vii| - the chariot", "|vii| - the chariot"],
+["|viii| - strength", "|viii| - strength"],
+["|ix| - the hermit", "|ix| - the hermit"],
+["|x| - wheel of fortune", "|x| - wheel of fortune"], 
+["|xi| - justice", "|xi| - justice"],
+["|xii| - the hanged man", "|xii| - the hanged man"],
+["|xiii| - death", "|xiii| - death"],
+["|xiv| - temperance", "|xiv| - temperance"],
+["|xv| - the devil", "|xv| - the devil"],
+["|xvi| - the tower", "|xvi| - the tower"],
+["|xvii| - the star", "|xvii| - the star"],
+["|xviii| - the moon", "|xviii| - the moon"],
+["|xix| - the sun", "|xix| - the sun"],
+["|xx| - judgment", "|xx| - judgment"], 
+["|xxi| - the world", "|xxi| - the world"],
+["|a| - ace of penticles", "|a| - ace of penticles"],
+["|2| - two of penticles", "|2| - two of penticles"],
+["|3| - three of penticles", "|3| - three of penticles"],
+["|4| - four of penticles", "|4| - four of penticles"],
+["|5| - five of penticles", "|5| - five of penticles"],
+["|6| - six of penticles", "|6| - six of penticles"],
+["|7| - seven of penticles", "|7| - seven of penticles"],
+["|8| - eight of penticles", "|8| - eight of penticles"],
+["|9| - nine of penticles", "|9| - nine of penticles"],
+["|10| - ten of penticles", "|10| - ten of penticles"],
+["|p| - page of pentacles", "|p| - page of pentacles"],
+["|j| - jack of penticles", "|j| - jack of penticles"],
+["|k| - king of penticles", "|k| - king of penticles"],
+["|q| - queen of penticles", "|q| - queen of penticles"],
+["|a| - ace of swords", "|a| - ace of swords"],
+["|2| - two of swords", "|2| - two of swords"],
+["|3| - three of swords", "|3| - three of swords"],
+["|4| - four of swords", "|4| - four of swords"],
+["|5| - five of swords", "|5| - five of swords"],
+["|6| - six of swords", "|6| - six of swords"],
+["|7| - seven of swords", "|7| - seven of swords"],
+["|8| - eight of swords", "|8| - eight of swords"],
+["|9| - nine of swords", "|9| - nine of swords"],
+["|10| - ten of swords", "|10| - ten of swords"],
+["|p| - page of swords", "|p| - page of swords"],
+["|j| - jack of swords", "|j| - jack of swords"],
+["|k| - king of swords", "|k| - king of swords"],
+["|q| - queen of swords", "|q| - queen of swords"],
+["|a| - ace of wands", "|a| - ace of wands"],
+["|2| - two of wands", "|2| - two of wands"],
+["|3| - three of wands", "|3| - three of wands"],
+["|4| - four of wands", "|4| - four of wands"],
+["|5| - five of wands", "|5| - five of wands"],
+["|6| - six of wands", "|6| - six of wands"],
+["|7| - seven of wands", "|7| - seven of wands"],
+|8| - eight of wands
+|9| - nine of wands
+|10| - ten of wands
+|p| - page of wands
+|j| - jack of wands
+|k| - king of wands
+|q| - queen of wands
+
+of cups
+|a| - ace of cups
+|2| - two of cups
+|3| - three of cups
+|4| - four of cups
+|5| - five of cups
+|6| - six of cups
+|7| - seven of cups
+|8| - eight of cups
+|9| - nine of cups
+|10| - ten of cups
+|p| - page of cups
+|j| - jack of cups
+|k| - king of cups
+|q| - queen of cups
